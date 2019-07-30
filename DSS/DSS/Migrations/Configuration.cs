f6a5c55231ff6fcfb2fdf95654a45b6543392ada
MFortal <mashfort@yandex.ru>
2019-07-30 15:54:41 +0300

    Удаление неиспоьзуемых директив

--- DSS/DSS/Migrations/Configuration.cs
+++ DSS/DSS/Migrations/Configuration.cs
@@ -2,10 +2,7 @@ namespace DSS.Migrations
 {
     using DSS.Models;
     using DSS.SourceSeed;
-    using System;
-    using System.Data.Entity;
     using System.Data.Entity.Migrations;
-    using System.Linq;
 
     internal sealed class Configuration : DbMigrationsConfiguration<DssContext>
     {
@@ -18,7 +15,7 @@ namespace DSS.Migrations
         {
             context.Categories.AddOrUpdate(CategoriesInitializer.Initialize());
             context.Subcategories.AddOrUpdate(SubcategoriesInitializer.Initialize());
-            
+
             context.SaveChanges();
         }
     }
--- DSS/DSS/Models/Value.cs
+++ DSS/DSS/Models/Value.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Web;
 
 namespace DSS.Models
 {
@@ -13,10 +10,10 @@ namespace DSS.Models
         public int Id { get; set; }
         [Required, Column("Value")]
         public string PropertyValue { get; set; }
-        
+
         public int PropertyId { get; set; }
 
-        public virtual Property Property{ get; set; }
+        public virtual Property Property { get; set; }
         public virtual ICollection<Cell> Cells { get; set; }
     }
 }
