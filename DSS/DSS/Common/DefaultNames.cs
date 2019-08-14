edc44a9fb3653e90c485c3c3fef5c56431365f4d
MFortal <mashfort@yandex.ru>
2019-08-14 20:49:29 +0300

    Добавление, редактирование, удаление категорий и субкатегорий

--- DSS/DSS/Common/DefaultNames.cs
+++ DSS/DSS/Common/DefaultNames.cs
@@ -11,5 +11,39 @@
         public const int RusId = 1;
 
         public const int CountVariance = 1;
+
+
+
+        public const string DeleteCategory = "Удаление категории";
+
+        public const string DeleteSubcategory = "Удаление подкатегории";
+
+        public const string Delete = "Удалить";
+
+        public const string Edit = "Редактировать";
+        
+        public const string EditCategory = "Редактирование категории";
+
+        public const string EditSubcategory = "Редактирование подкатегории";
+
+        public const string CreateCategory = "Добавление категории";
+
+        public const string CreateSubcategory = "Добавление подкатегории";
+
+        public const string Back = "Обратно";
+
+        public const string Details = "Детали";
+
+        public const string CatalogComponents = "Каталог компонентов";
+
+
+        public const string Name = "Наименование";
+
+        public const string Image = "Изображение";
+
+        public const string Category = "Категория";
+
+        public const string Subcategories = "Подкатегории";
+
     }
 }
---
+++ DSS/DSS/Controllers/SubcategoriesController.cs
@@ -0,0 +1,105 @@
+﻿using System.Data.Entity;
+using System.Net;
+using System.Web.Mvc;
+using DSS.Models;
+
+namespace DSS.Controllers
+{
+    public class SubcategoriesController : Controller
+    {
+        private DssContext db = new DssContext();
+
+        // GET: Subcategories/Create
+        public ActionResult Create()
+        {
+            ViewBag.CategoryId = new SelectList(db.Categories, "Id", "Name");
+            return View();
+        }
+
+        // POST: Subcategories/Create
+        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
+        // сведения см. в статье https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Image,CategoryId")] Subcategory subcategory)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Subcategories.Add(subcategory);
+                db.SaveChanges();
+                return RedirectToAction("Index", "Categories");
+            }
+
+            ViewBag.CategoryId = new SelectList(db.Categories, "Id", "Name", subcategory.CategoryId);
+            return View(subcategory);
+        }
+
+        // GET: Subcategories/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Subcategory subcategory = db.Subcategories.Find(id);
+            if (subcategory == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CategoryId = new SelectList(db.Categories, "Id", "Name", subcategory.CategoryId);
+            return View(subcategory);
+        }
+
+        // POST: Subcategories/Edit/5
+        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
+        // сведения см. в статье https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Image,CategoryId")] Subcategory subcategory)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(subcategory).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index", "Categories");
+            }
+            ViewBag.CategoryId = new SelectList(db.Categories, "Id", "Name", subcategory.CategoryId);
+            return View(subcategory);
+        }
+
+        // GET: Subcategories/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Subcategory subcategory = db.Subcategories.Find(id);
+            if (subcategory == null)
+            {
+                return HttpNotFound();
+            }
+            return View(subcategory);
+        }
+
+        // POST: Subcategories/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Subcategory subcategory = db.Subcategories.Find(id);
+            db.Subcategories.Remove(subcategory);
+            db.SaveChanges();
+            return RedirectToAction("Index", "Categories");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
