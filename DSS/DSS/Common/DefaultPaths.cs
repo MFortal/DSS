691b828f2de855370cebcda5331329ef7707a17f
Nikolay Latsyn <latsynn@mail.ru>
2019-08-20 22:24:25 +0300

    Добавление имен в константы

--- DSS/DSS/Common/DefaultPaths.cs
+++ DSS/DSS/Common/DefaultPaths.cs
@@ -4,6 +4,18 @@
     {
         public const string CategoriesImagesPath = "../../Images/Categories/";
 
+        public const string BlobsImagesPath = "../../Images/Blobs/";
+
+        public const string HomeImageName = "image.jpg";
+
+        public const string Contact1ImageName = "contact1.jpg";
+
+        public const string Contact2ImageName = "contact2.jpg";
+
+        public const string Contact3ImageName = "contact3.jpg";
+
+        public const string MainIconImageName = "icon.ico";
+
         public static string ToRootPath(this string path) => path.Replace("../", "").Insert(0, "~/");
     }
 }
