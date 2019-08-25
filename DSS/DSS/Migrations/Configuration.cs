6fde4f35ea0556c10e93e981f86d8e0f987bb268
Лацын Николай <Nikolay.Latsyn@firstlinesoftware.com>
2019-08-25 23:18:42 +0300

    Рандом для Cells

--- DSS/DSS/Migrations/Configuration.cs
+++ DSS/DSS/Migrations/Configuration.cs
@@ -13,15 +13,16 @@ namespace DSS.Migrations
 
         protected override void Seed(DssContext context)
         {
-            //context.Categories.AddOrUpdate(CategoriesInitializer.Initialize());
-            //context.Subcategories.AddOrUpdate(SubcategoriesInitializer.Initialize());
-            //context.Countries.AddOrUpdate(CountriesInitializer.Initialize());
-            //context.Components.AddOrUpdate(ComponentsInitializer.Initialize());
-            //context.Properties.AddOrUpdate(PropertiesInitializer.Initialize());
-            //context.SubcategoryProperties.AddOrUpdate(SubcategoryPropertiesInitializer.Initialize());
-            //context.Values.AddOrUpdate(ValuesInitializer.Initialize());
-            //context.Cells.AddOrUpdate(CellsInitializer.Initialize());            
+            context.Categories.AddOrUpdate(CategoriesInitializer.Initialize());
+            context.Subcategories.AddOrUpdate(SubcategoriesInitializer.Initialize());
+            context.Countries.AddOrUpdate(CountriesInitializer.Initialize());
+            context.Components.AddOrUpdate(ComponentsInitializer.Initialize());
+            context.Properties.AddOrUpdate(PropertiesInitializer.Initialize());
+            context.SubcategoryProperties.AddOrUpdate(SubcategoryPropertiesInitializer.Initialize());
+            context.Values.AddOrUpdate(ValuesInitializer.Initialize());
 
+            context.SaveChanges();
+            context.Cells.AddOrUpdate(CellsInitializer.Initialize(context));       
             context.SaveChanges();
         }
     }
--- DSS/DSS/SourceSeed/CellsInitializer.cs
+++ DSS/DSS/SourceSeed/CellsInitializer.cs
@@ -1,47 +1,83 @@
-﻿using DSS.Models;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using DSS.Models;
 
 namespace DSS.SourceSeed
 {
     public class CellsInitializer
     {
-        public static Cell[] Initialize()
+        public static Cell[] Initialize(DssContext db)
         {
-            return new Cell[]
+            var rand = new Random();
+            var result = new List<Cell>();
+
+            foreach (var component in db.Components)
             {
-                new Cell{Id=1, ComponentId=1, ValueId=1},
-                new Cell{Id=2, ComponentId=1, ValueId=4},
+                //выбор всех свойст подкатегории
+                var propertyIds = db.SubcategoryProperties
+                    .Where(x => component.SubcategoryId == x.SubcategoryId)
+                    .Select(x => x.PropertyId);
+                var properties = db.Properties
+                    .Where(x => propertyIds.Contains(x.Id))
+                    .Where(x => x.IsEnum);
 
-                new Cell{Id=3, ComponentId=2, ValueId=2},
-                new Cell{Id=4, ComponentId=2, ValueId=3},
-                new Cell{Id=5, ComponentId=2, ValueId=8},
+                foreach (var property in properties)
+                {
+                    var values = db.Values
+                        .Where(x => property.Id == x.PropertyId)
+                        .Select(x => x.Id)
+                        .ToArray();
 
-                new Cell{Id=6, ComponentId=3, ValueId=1},
-                new Cell{Id=7, ComponentId=3, ValueId=5},
-                new Cell{Id=8, ComponentId=3, ValueId=9},
+                    if (values.Any())
+                    {
+                        result.Add(new Cell()
+                        {
+                            ComponentId = component.Id,
+                            ValueId = values[rand.Next(0, values.Length)]
+                        });
+                    }                    
+                }
+            }
 
-                new Cell{Id=9, ComponentId=4, ValueId=1},
+            return result.ToArray();
 
-                new Cell{Id=10, ComponentId=5, ValueId=1},
+            //return new Cell[]
+            //{
+            //    new Cell{Id=1, ComponentId=1, ValueId=1},
+            //    new Cell{Id=2, ComponentId=1, ValueId=4},
 
-                new Cell{Id=11, ComponentId=6, ValueId=4},
+            //    new Cell{Id=3, ComponentId=2, ValueId=2},
+            //    new Cell{Id=4, ComponentId=2, ValueId=3},
+            //    new Cell{Id=5, ComponentId=2, ValueId=8},
 
-                new Cell{Id=12, ComponentId=7, ValueId=3},
-                new Cell{Id=13, ComponentId=7, ValueId=8},
-                new Cell{Id=22, ComponentId=7, ValueId=15},
+            //    new Cell{Id=6, ComponentId=3, ValueId=1},
+            //    new Cell{Id=7, ComponentId=3, ValueId=5},
+            //    new Cell{Id=8, ComponentId=3, ValueId=9},
 
-                new Cell{Id=14, ComponentId=8, ValueId=10},
-                new Cell{Id=21, ComponentId=8, ValueId=14},
+            //    new Cell{Id=9, ComponentId=4, ValueId=1},
 
-                new Cell{Id=15, ComponentId=9, ValueId=4},
-                new Cell{Id=16, ComponentId=9, ValueId=7},
-                new Cell{Id=17, ComponentId=9, ValueId=13},
+            //    new Cell{Id=10, ComponentId=5, ValueId=1},
 
-                new Cell{Id=18, ComponentId=13, ValueId=17},
+            //    new Cell{Id=11, ComponentId=6, ValueId=4},
 
-                new Cell{Id=19, ComponentId=14, ValueId=17},
+            //    new Cell{Id=12, ComponentId=7, ValueId=3},
+            //    new Cell{Id=13, ComponentId=7, ValueId=8},
+            //    new Cell{Id=22, ComponentId=7, ValueId=15},
 
-                new Cell{Id=20, ComponentId=15, ValueId=18}
-            };
+            //    new Cell{Id=14, ComponentId=8, ValueId=10},
+            //    new Cell{Id=21, ComponentId=8, ValueId=14},
+
+            //    new Cell{Id=15, ComponentId=9, ValueId=4},
+            //    new Cell{Id=16, ComponentId=9, ValueId=7},
+            //    new Cell{Id=17, ComponentId=9, ValueId=13},
+
+            //    new Cell{Id=18, ComponentId=13, ValueId=17},
+
+            //    new Cell{Id=19, ComponentId=14, ValueId=17},
+
+            //    new Cell{Id=20, ComponentId=15, ValueId=18}
+            //};
         }
     }
 }
